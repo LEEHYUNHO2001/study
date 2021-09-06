47a6c4aeeecdc3fa0b44b6cfaaf280c53a5b8a76
HyunHo Lee <dlgusgh2001@naver.com>
2021-09-06 18:13:46 +0900

    Next Server Side Rendering init

--- front/reducers/index.js
+++ front/reducers/index.js
@@ -5,17 +5,19 @@ import user from './user';
 import post from './post';
 
 //이전상태와 액션을 이용하여 다음상태를 만듬
-const rootReducer = combineReducers({ index: (state = {}, action) => {
-        switch(action.type){
-            case HYDRATE:
-                return{...state, ...action.payload};
-                
-                default:
-                    return state;
-        };
-    },
-    user,
-    post
-})
+const rootReducer = (state, action) => {
+    switch(action.type){
+        case HYDRATE:
+            console.log('HYDRATE', action);
+            return action.payload;
+        default:{
+            const combineReducer = combineReducers({
+                user,
+                post,
+            });
+            return combineReducer(state, action)
+        }
+    }
+};
 
 export default rootReducer;
