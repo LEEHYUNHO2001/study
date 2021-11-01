a5e637354dda397a56ac9e209c7ce463cc3e7b4a
HyunHo Lee <dlgusgh2001@naver.com>
2021-11-01 14:15:40 +0900

    SearchContainer SearchPresenter

--- hoflix/src/Routes/Search/SearchContainer.js
+++ hoflix/src/Routes/Search/SearchContainer.js
@@ -10,20 +10,29 @@ export default class extends React.Component{
         error: null,
         loading: false,
     };
-
+/*
     componentDidMount(){
         this.handleSubmit();
     }
-
-    handleSubmit = () => {
+*/
+    handleSubmit = (event) => {
+        event.preventDefault();
         const {searchTerm} = this.state;
         if(searchTerm !== ""){
             this.searchByTerm();
         }
+    };
+
+    updateTerm = (event) => {
+        const {target: {value}} = event;
+        this.setState({
+            searchTerm: value,
+        });
     }
+
     searchByTerm = async() => {
         const {searchTerm} = this.state;
-        this.setState({loading: true})
+        this.setState({loading: true});
         try{
             const {data: {results: movieResults}} = await moviesApi.search(searchTerm);
             const {data: {results: tvResults}} = await tvApi.search(searchTerm);
@@ -36,12 +45,13 @@ export default class extends React.Component{
         } finally{
             this.setState({loading: false});
         }
-    }
+    };
 
     render() {
         const {movieResults, tvResults, searchTerm, error, loading} = this.state;
         return <SearchPresenter movieResults={movieResults} 
         tvResults={tvResults} searchTerm={searchTerm}
-        error={error} loading={loading} handleSubmit={this.handleSubmit}/>
+        error={error} loading={loading} 
+        handleSubmit={this.handleSubmit} updateTerm={this.updateTerm}/>
     }
 }
--- hoflix/src/Routes/Search/SearchPresenter.js
+++ hoflix/src/Routes/Search/SearchPresenter.js
@@ -1,8 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
+import Section from "Components/Section";
 
-const SearchPresenter = ({ movieResults,tvResults,searchTerm,handleSubmit,error,loading}) => null;
+const Container = styled.div`
+    padding: 0px 20px;
+`;
+
+const Form = styled.form`
+    margin-bottom: 50px;
+    width: 100%;
+`;
+
+const Input = styled.input`
+    all: unset;
+    font-size: 28px;
+    width: 100%;
+`;
+
+const SearchPresenter = ({ movieResults,tvResults,searchTerm,
+    handleSubmit,error,loading, updateTerm}) => <Container>
+    <Form onSubmit={handleSubmit}>
+        <Input placeholder="Search Movies or TV Show.." 
+        value={searchTerm} onChange={updateTerm}/>
+    </Form>
+    {loading ? <Loader /> 
+    : <>
+        {movieResults && movieResults.length > 0 && 
+        <Section title="Movie Results">
+            {movieResults.map(movie => <span key={movie.id}>{movie.title}</span>)}    
+        </Section>}
+        {tvResults && tvResults.length > 0 && 
+        <Section title="TV Show Results">
+            {tvResults.map(show => <span key={show.id}>{show.name}</span>)}    
+        </Section>}
+    </>}
+</Container>;
 
 SearchPresenter.propTypes = {
     movieResults:PropTypes.array,
@@ -10,7 +44,8 @@ SearchPresenter.propTypes = {
     error:PropTypes.string,
     loading:PropTypes.bool.isRequired,
     searchTerm:PropTypes.string,
-    handleSubmit:PropTypes.func.isRequired
+    handleSubmit:PropTypes.func.isRequired,
+    updateTerm: PropTypes.func.isRequired
 }
 
 export default SearchPresenter;
