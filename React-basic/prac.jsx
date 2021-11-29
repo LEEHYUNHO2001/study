0253faf9807c56ff20e8e4133a5816602be2b534
HyunHo Lee <dlgusgh2001@naver.com>
2021-11-29 22:04:08 +0900

    React JSX

--- React-basic/prac.jsx
+++ React-basic/prac.jsx
@@ -1,12 +1,4 @@
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-  }
-  
-  const user = {
-    firstName: 'Harper',
-    lastName: 'Perez',
-  };
-  
-  const element = <h1>Hello, {formatName(user)}!</h1>;
-  
-  ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
