e4ae342f1b0a70c5ce788fd35ae6454b5a6d06a8
HyunHo Lee <dlgusgh2001@naver.com>
2021-12-15 21:54:58 +0900

    btn event

--- src/js/main.js
+++ src/js/main.js
@@ -13,18 +13,70 @@ const city = document.querySelector(".city-list");
 const btnJob = document.querySelector(".btn-job");
 const job = document.querySelector(".job-list");
 
-const none = document.querySelector(".btn-off");
+const relatedBtn = document.querySelector(".related-list");
+const relatedList = document.querySelector(".related-items");
+
+const btnIns = document.querySelector(".btn-ins");
+const insList = document.querySelector(".insta-list");
+
+const btnFace = document.querySelector(".btn-face");
+const faceList = document.querySelector(".face-list");
+
+const btnGit = document.querySelector(".btn-git");
+const inList = document.querySelector(".in-list");
+
+const btnYou = document.querySelector(".btn-you");
+const youList = document.querySelector(".you-list");
+
+const arr = [
+  lang,
+  search,
+  region,
+  city,
+  job,
+  relatedList,
+  insList,
+  faceList,
+  inList,
+  youList,
+];
+
+const none = document.querySelectorAll(".btn-off");
 let flag = false;
 
 btnLang.addEventListener("click", () => {
+  if (flag) {
+    for (let i = 0; i < arr.length; i++) {
+      if (!arr[i].document.querySelector(".btn-off")) {
+        arr[i].classList.add("btn-off");
+      }
+    }
+  }
+  flag = !flag;
   lang.classList.toggle("btn-off");
 });
 
 btnSearch.addEventListener("click", () => {
+  if (flag) {
+    for (let i = 0; i < arr.length; i++) {
+      if (!arr[i].document.querySelector(".btn-off")) {
+        arr[i].classList.add("btn-off");
+      }
+    }
+  }
+  flag = !flag;
   search.classList.toggle("btn-off");
 });
 
 btnRegion.addEventListener("click", () => {
+  if (flag) {
+    for (let i = 0; i < arr.length; i++) {
+      if (!arr[i].document.querySelector(".btn-off")) {
+        arr[i].classList.add("btn-off");
+      }
+    }
+  }
+  flag = !flag;
   region.classList.toggle("btn-off");
 });
 
@@ -34,4 +86,24 @@ btnCity.addEventListener("click", () => {
 
 btnJob.addEventListener("click", () => {
   job.classList.toggle("btn-off");
+});
+
+relatedBtn.addEventListener("click", () => {
+  relatedList.classList.toggle("btn-off");
+});
+
+btnIns.addEventListener("click", () => {
+  insList.classList.toggle("btn-off");
+});
+
+btnFace.addEventListener("click", () => {
+  faceList.classList.toggle("btn-off");
+});
+
+btnGit.addEventListener("click", () => {
+  inList.classList.toggle("btn-off");
+});
+
+btnYou.addEventListener("click", () => {
+  youList.classList.toggle("btn-off");
 });
