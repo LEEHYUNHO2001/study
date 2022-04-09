6a69605d16a5199a74d6a4217d89c985be04c080
HyunHo Lee <dlgusgh2001@naver.com>
2022-04-09 23:57:15 +0900

    🎉 프로젝트 초기 셋팅 - clayful연동

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import clayful from "clayful/client-js";
+import axios from "axios";
+
+clayful.config({
+  client:
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjMxNzdlYmU0MzEyNDQzZTZkMjFhZTc0Nzg5ODE3ZTU2NDRlMjAzOGRhNTc5ZGI2OGNjYTAzM2Y4MGU5MjIxMGIiLCJyb2xlIjoiY2xpZW50IiwiaWF0IjoxNjQ5NTE0NzY1LCJzdG9yZSI6IlkyQlVKVzRUQlA3VC5EUlY2OUxBMjVDVkQiLCJzdWIiOiI2NlVSNU5QSDU4NEUifQ.oih6dG_sJMoTRHRdNOwbsTDtLE78wjNBcFFatXDOPvI",
+});
+clayful.install("request", require("clayful/plugins/request-axios")(axios));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,30 @@
+import clayful from "clayful/client-js";
+
+const LandingPage = () => {
+  const Product = clayful.Product;
+
+  const options = {
+    query: {
+      page: 1,
+    },
+  };
+
+  Product.list(options, (err, res) => {
+    if (err) {
+      console.log(err.isClayful);
+      console.log(err.model);
+      console.log(err.method);
+      console.log(err.status);
+      console.log(err.headers);
+      console.log(err.code);
+      console.log(err.message);
+    }
+
+    console.log(res.status);
+    console.log(res.headers);
+    console.log(res.data);
+  });
+  return <div>LandingPage</div>;
+};
+
+export default LandingPage;
