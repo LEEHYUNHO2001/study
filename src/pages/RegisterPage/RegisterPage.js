c199ab38e3a57d27f8d89e71b859c5fcea0ba1e6
HyunHo Lee <dlgusgh2001@naver.com>
2022-04-10 16:43:58 +0900

    ✨회원가입

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import clayful from "clayful/client-js";
 
 const RegisterPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password.length < 8)
+      return alert("비밀먼호는 8자리 이상이어야 합니다.");
+    const Customer = clayful.Customer;
+    const payload = {
+      email,
+      password,
+    };
+
+    Customer.createMe(payload, (err, res) => {
+      if (err) console.log(err.code);
+      const data = res.data;
+      console.log(data);
+    });
+  };
+  const handleInput = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <Container>
       <Title>회원가입</Title>
-      <LoginForm>
-        <input placeholder="Apple ID" type="email" name="email" value="" />
+      <LoginForm onSubmit={handleSubmit}>
         <input
+          onChange={handleInput}
+          placeholder="Apple ID"
+          type="email"
+          name="email"
+          value={email}
+        />
+        <input
+          onChange={handlePassword}
           placeholder="Password"
           type="password"
           name="password"
-          value=""
+          value={password}
         />
         <LoginSubmit type="submit">회원가입</LoginSubmit>
         <Link to="login">이미 Apple ID가 있다면 바로 로그인 하세요.</Link>
