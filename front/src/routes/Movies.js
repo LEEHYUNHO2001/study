c09a3e998b0fa92cede067506c712004dcca8e5f
HyunHo Lee <dlgusgh2001@naver.com>
2022-07-25 17:33:17 +0900

    useQuery 사용

--- front/src/routes/Movies.js
+++ front/src/routes/Movies.js
@@ -1,7 +1,34 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 
+const ALL_TWEETS = gql`
+  query getTweets {
+    allTweets {
+      id
+      text
+      author {
+        fullname
+      }
+    }
+    # 다른 query 추가 가능
+  }
+`;
+
 const Movies = () => {
-  return <div>Movies</div>;
+  const { data: tweets, loading, error } = useQuery(ALL_TWEETS);
+
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div>Error...</div>;
+
+  return (
+    <ul>
+      {tweets.allTweets.map((tweet) => {
+        const { id, text } = tweet;
+        return <li key={`tweet-${id}`}>{text}</li>;
+      })}
+    </ul>
+  );
 };
 
 export default Movies;
